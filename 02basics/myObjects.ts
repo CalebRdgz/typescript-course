07b0000792402751f1c9032062cb5967eddcf0a7
CalebRdgz <calebrodgz@gmail.com>
2024-03-07 13:05:52 -0600

    type aliases

---
+++ 02basics/myObjects.ts
@@ -0,0 +1,44 @@
+// //object of a User:
+// const User = {
+//     name: "caleb",
+//     email: "caleb@email.com",
+//     isActive: true
+// }
+
+// //use objects by passing them through a function:
+// //taking in an object with name and isPaid properties:
+// function createUser({name: string, isPaid: boolean}) {}
+
+// //creating a new user with newUser variable taking in more properties:
+// let newUser = { name: "caleb", isPaid: false, email: "c@c.com" };
+
+// createUser(newUser);
+
+// function createCourse (): {name: string, price: number} {
+//     return {
+//         name: "reactjs",
+//         price: 399
+//     }
+// }
+
+// //create an app where we onboard a user, user has some properties well go through
+// //when someone takes a parameter, they need all the info of the user itself
+// //create a types of and define the user:
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean;
+// }
+
+// //giving these types^ we created to the input user in this function:
+// //return type is also User((user: User): "User"):
+// function createUser(user: User): User {
+//     return { name: "", email: "", isActive: true };
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
+
+
+
+export {}
