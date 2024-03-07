ce8c1c4767ed056438ec992186b8bb97a59bd2b5
CalebRdgz <calebrodgz@gmail.com>
2024-03-07 13:36:33 -0600

    array types

---
+++ 02basics/myArray.ts
@@ -0,0 +1,23 @@
+//string[] = input type should be string array
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string;
+    isActive: boolean;
+}
+
+const allUsers: User[] = []
+
+//array inside array:
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+superHeroes.push("spiderman")
+heroPower.push(2);
+
+//the object should follow the type User rules:
+allUsers.push({name: "", isActive: true})
