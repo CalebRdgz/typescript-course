9c422c01c1b124291d64ea418e652265790cb912
CalebRdgz <calebrodgz@gmail.com>
2024-03-08 15:31:47 -0600

    Generics

--- 03purets/dist/myGenerics.js
+++ 03purets/dist/myGenerics.js
@@ -1,1 +1,28 @@
 "use strict";
+//create a number type array that stores scores:
+const score = [];
+//create a string type array that stores names:
+const names = [];
+//create an identity that accepts a val of boolean or number:
+function identityOne(val) {
+    return val;
+}
+//DONT USE ANY
+function identityTwo(val) {
+    return val;
+}
+//the actual topic:
+//any could be ANYTHING, Type almost like any, but <Type> actually logs the type of that value
+function identityThree(val) {
+    return val;
+}
+//pass number 3 (returns the actual number type of 3):
+identityThree(3);
+//returns an actual boolean:
+identityThree(true);
+//T (or any letter) is shortcut for Type:
+function identityFour(val) {
+    return val;
+}
+//use the Bottle type we created^
+identityFour({ brand: "caleb", type: 3 });
--- 03purets/src/myGenerics.js
+++ 03purets/src/myGenerics.js
@@ -1,39 +1,27 @@
 //create a number type array that stores scores:
-const score: Array<number> = [];
+var score = [];
 //create a string type array that stores names:
-const names: Array<string> = [];
-
+var names = [];
 //create an identity that accepts a val of boolean or number:
-function identityOne(val: boolean | number): boolean | number {
-  return val;
+function identityOne(val) {
+    return val;
 }
-
 //DONT USE ANY
-function identityTwo(val: any): any {
-  return val;
+function identityTwo(val) {
+    return val;
 }
-
 //the actual topic:
 //any could be ANYTHING, Type almost like any, but <Type> actually logs the type of that value
-function identityThree<Type>(val: Type): Type {
-  return val;
+function identityThree(val) {
+    return val;
 }
-
 //pass number 3 (returns the actual number type of 3):
 identityThree(3);
 //returns an actual boolean:
 identityThree(true);
-
 //T (or any letter) is shortcut for Type:
-function identityFour<T>(val: T): T {
-  return val;
+function identityFour(val) {
+    return val;
 }
-
-//create your own Bottle type:
-interface Bottle {
-  brand: string;
-  type: number;
-}
-
 //use the Bottle type we created^
-identityFour<Bottle>({brand: "caleb", type: 3});
+identityFour({ brand: "caleb", type: 3 });
